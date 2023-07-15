42a9f5d19078c63990dded8be083ce14dea99148
Andrea Botello <andrea.botellor@misena.edu.co>
2023-07-15 09:37:06 -0500

    AD

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,7 +7,7 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/arrastrar.svg", alt="Arrastrar y soltar")
+            img(src="@/assets/template/arrastrar.svg", alt="")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo.color-acento-contenido
           h2 Verificando las emisiones de la producción
@@ -15,7 +15,7 @@
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Arrastrar y soltar
+              p.fw-bold.mb-0 Completar espacios
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
